f46ba0f7c267bb452594c039e91bd3b6c5cd16d7
MarioSevilla <oficialmariosevilla@icloud.com>
2024-03-15 19:03:12 -0600

    update: modify some styles

--- src/app/components/finanzas/finanzas.component.ts
+++ src/app/components/finanzas/finanzas.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-finanzas',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule, MatIconModule, MatCheckboxModule, MatExpansionModule],
   templateUrl: './finanzas.component.html',
   styleUrl: './finanzas.component.scss'
 })
